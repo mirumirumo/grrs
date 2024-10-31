aa6b5abd2d512bc6f8a51ccffa55be7cd37a08d5
mirumirumo <yoshinouchi.m.aa@gmail.com>
2024-11-01 00:00:40 +0900

    introduce clap lib

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-use std::env::args;
+use clap::Parser;
 
+#[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
+
 fn main() {
-    let pattern = args().nth(1).expect("no pattern given");
-    let path = args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
